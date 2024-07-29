546a19df058cc168224edf66d8df2cbc574611a6
Oonet96 <captaing96@gmail.com>
2024-07-29 17:09:38 +0900

    24.07.29 HT :  build 다른 프로젝트 연결

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './_practice_team_project_toreact/App.jsx';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
